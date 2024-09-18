6981e3981f1ba3f906aa38884cfda1fd5f02d640
flaggton <markusvogel12@gmail.com>
2024-09-18 22:20:01 +0200

    impl zoomableScrollPane

--- src/main/java/io/flaggton/pixxelator/views/MainController.java
+++ src/main/java/io/flaggton/pixxelator/views/MainController.java
@@ -1,8 +1,12 @@
 package io.flaggton.pixxelator.views;
 
+import com.wedasoft.wedasoftFxCustomNodes.zoomableScrollPane.ZoomableScrollPane;
 import io.flaggton.pixxelator.services.JfxUiService;
 import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -15,15 +19,21 @@ import java.util.ResourceBundle;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @RequiredArgsConstructor
 public class MainController implements Initializable {
+    @FXML
+    private BorderPane borderPane;
 
     private final JfxUiService jfxUiService;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Button button = new Button();
+        ZoomableScrollPane zoomableScrollPane = new ZoomableScrollPane(button);
+        borderPane.setCenter(zoomableScrollPane);
     }
-
     public void onExitMenuItemClick() {
         Platform.exit();
         System.exit(0);
     }
 }
+
