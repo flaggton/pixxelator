81a7ca47b8d49f53b582c14e595049e2c8e59533
flaggton <markusvogel12@gmail.com>
2024-10-09 23:00:37 +0200

    Impl drawing feature on Pixel Drawing Canvas

--- src/main/java/io/flaggton/pixxelator/models/PixelDrawingPane.java
+++ src/main/java/io/flaggton/pixxelator/models/PixelDrawingPane.java
@@ -2,6 +2,7 @@ package io.flaggton.pixxelator.models;
 
 import com.wedasoft.wedasoftFxCustomNodes.zoomableScrollPane.ZoomableScrollPane;
 import io.flaggton.pixxelator.enums.DrawingMode;
+import javafx.application.Platform;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -10,6 +11,8 @@ import java.util.Random;
 
 public class PixelDrawingPane extends ZoomableScrollPane implements DrawingPaneActions {
     Random random = new Random();
+    private Color selectedColor = Color.WHITE;
+    private DrawingMode drawingMode;
 
     public PixelDrawingPane(int widthInPx, int heightInPx) {
         super(new GridPane());
@@ -23,19 +26,30 @@ public class PixelDrawingPane extends ZoomableScrollPane implements DrawingPaneA
 
     private Rectangle createRectangle() {
         Rectangle rectangle = new Rectangle();
-        rectangle.setWidth(1);
-        rectangle.setHeight(1);
-        rectangle.setFill(Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble()));
+        rectangle.setWidth(10);
+        rectangle.setHeight(10);
+//        rectangle.setFill(Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble()));
+        rectangle.setOnMouseMoved(mouseEvent -> onMouseDragged(rectangle));
+
         return rectangle;
     }
 
     @Override
     public void setDrawingMode(DrawingMode drawingMode) {
         System.out.println("PixelDrawingPane drawingMode = " + drawingMode);
+        this.drawingMode = drawingMode;
     }
 
     @Override
     public void setColor(Color selectedColor) {
         System.out.println("PixelDrawingPane selectedColor = " + selectedColor.toString());
+        this.selectedColor = selectedColor;
+    }
+
+    public void onMouseDragged(Rectangle rectangle) {
+        System.out.println("x");
+        Platform.runLater(() -> {
+            rectangle.setFill(selectedColor);
+        });
     }
 }
